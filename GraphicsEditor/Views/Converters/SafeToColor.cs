64f16d0204644ff9a742d4eec47d5893234a55fa
Денис Мурашов <denismur.nsk@gmail.com>
2023-03-19 23:54:52 +0700

    Добавьте файлы проекта.

---
+++ GraphicsEditor/Views/Converters/SafeToColor.cs
@@ -0,0 +1,17 @@
+using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace GraphicsEditor.Views.Converters {
+    
+    internal class SafeToColor: IValueConverter {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+            if (value == null) return null;
+            
+            throw new NotSupportedException();
+        }
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
